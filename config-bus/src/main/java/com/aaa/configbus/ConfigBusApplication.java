e1c4820ec5668be5f391d633970ce5872be0a18e
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-03-18 18:15:32 +0800

    spring-cloud-config 初始化项目

---
+++ config-bus/src/main/java/com/aaa/configbus/ConfigBusApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.configbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigBusApplication.class, args);
+    }
+
+}
---
+++ config-server/src/test/java/com/aaa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
