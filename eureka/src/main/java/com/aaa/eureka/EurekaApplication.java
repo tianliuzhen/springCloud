71704ad2609df7008aa2d0e9d19c52254f37c647
tianliuzhen <tlzallen@163.com>
2020-03-19 22:57:11 +0800

    eureka取消安全认证

--- eureka/src/main/java/com/aaa/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/aaa/eureka/EurekaApplication.java
@@ -1,10 +1,16 @@
 package com.aaa.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+
+/**
+ * @author TLZ
+ */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
