c9b753a4f9744ea2252d1c337d81e9c828f1c38b
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-03-28 15:26:32 +0800

    增加配置thymeleaf 详细信息

--- boot-thymeleaf/src/main/java/com/aaa/bootthymeleaf/web/HtmlController.java
+++ boot-thymeleaf/src/main/java/com/aaa/bootthymeleaf/web/HtmlController.java
@@ -20,7 +20,7 @@ public class HtmlController {
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
         // 这里的路径指的是   resources/templates/test.html
-        return "test";
+        return "/test";
     }
 
 
