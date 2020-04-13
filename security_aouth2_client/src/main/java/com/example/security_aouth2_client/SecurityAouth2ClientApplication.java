30f904893633eb8a7267ec268d82de25d7909e7b
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-04-13 14:41:07 +0800

    简化 oauth2 client客户端pom配置

--- security_aouth2_client/src/main/java/com/example/security_aouth2_client/SecurityAouth2ClientApplication.java
+++ security_aouth2_client/src/main/java/com/example/security_aouth2_client/SecurityAouth2ClientApplication.java
@@ -1,7 +1,5 @@
 package com.example.security_aouth2_client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
