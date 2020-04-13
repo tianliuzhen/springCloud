1dc7b6382afcdd2998dcc97f95c12ba841e3fbf3
tianliuzhen <tianliuzhen@haoxiaec.com>
2020-04-13 11:55:10 +0800

    zuul 2.0 配置完善

--- zuul/src/main/java/com/aaa/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/aaa/zuul/ZuulApplication.java
@@ -1,19 +1,17 @@
 package com.aaa.zuul;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableZuulProxy
 @EnableEurekaClient
-@EnableResourceServer
 @SpringBootApplication
 public class ZuulApplication {
     private static final Logger log = LoggerFactory.getLogger(ZuulApplication.class);
