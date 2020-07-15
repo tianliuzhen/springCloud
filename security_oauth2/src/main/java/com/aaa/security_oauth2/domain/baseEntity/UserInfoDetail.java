3978f9e572e113516def26d2296a16730fd753c7
tianliuzhen <tlzallen@163.com>
2020-07-15 22:04:46 +0800

    authorities 增加返回值

--- security_oauth2/src/main/java/com/aaa/security_oauth2/domain/baseEntity/UserInfoDetail.java
+++ security_oauth2/src/main/java/com/aaa/security_oauth2/domain/baseEntity/UserInfoDetail.java
@@ -29,7 +29,7 @@ public class UserInfoDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
