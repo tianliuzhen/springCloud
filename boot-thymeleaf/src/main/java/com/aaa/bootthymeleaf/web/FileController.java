9a00231027d1ed635b413e3c0be3836c502acedd
tianliuzhen <tlzallen@163.com>
2020-11-01 00:19:32 +0800

    图片上传

---
+++ boot-thymeleaf/src/main/java/com/aaa/bootthymeleaf/web/FileController.java
@@ -0,0 +1,52 @@
+package com.aaa.bootthymeleaf.web;
+
+/**
+ * 文件上传
+ * @author liuzhen.tian
+ * @version 1.0 FileController.java  2020/10/31 22:54
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.UUID;
+
+@Controller
+public class  FileController {
+
+    private String filePath;
+
+    @GetMapping(value = "/file")
+    public String file() {
+        return "fileUpload";
+    }
+
+    @PostMapping(value = "/fileUpload")
+    public String fileUpload(@RequestParam(value = "file") MultipartFile file, Model model, HttpServletRequest request) {
+        if (file.isEmpty()) {
+            System.out.println("文件为空");
+        }
+        String fileName = file.getOriginalFilename();  // 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        String filePath = "D://temp-rainy//"; // 上传后的路径
+        fileName = UUID.randomUUID() + suffixName; // 新文件名
+        File dest = new File(filePath + fileName);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String filename = "/temp-rainy/" + fileName;
+        model.addAttribute("filename", filename);
+        return "fileUpload";
+    }
+
+
+}
+
